16c9366482f839001b9ecd29a3ca8aef167ef842
Erik Hanchett <erik.hanchett@gmail.com>
2020-02-28 16:25:13 -0800

    second try

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production" ? "/Vue360TestExamplePages" : "/",
+    process.env.NODE_ENV === "production" ? "/Vue360TestExamplePages/" : "/",
   outputDir: "docs"
 };
